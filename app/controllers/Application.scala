b5ac83cbdfc18729daa80cc1c65290a7a3553f2b
Muyinda ROgers <moverr@gmail.com>
2020-06-26 23:10:58 +0300

    Debuggging

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,12 +1,5 @@
 package controllers
 
-import play.api._
-import play.api.mvc._
-import play.api.cache.Cache
-import play.api.Play.current
-
-import play.api.db._
-
 object Application extends Controller {
 
   def index = Action {
