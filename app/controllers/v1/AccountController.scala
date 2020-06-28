d60fc05d14a818839845f7edf097fe295a01ac23
Muyinda ROgers <moverr@gmail.com>
2020-06-29 02:57:26 +0300

    :Debuggging

--- app/controllers/v1/AccountController.scala
+++ app/controllers/v1/AccountController.scala
@@ -1,5 +1,30 @@
 package controllers.v1
 
-class AccountController {
+import javax.inject.Inject
+import play.api.Logger
+import play.api.data.Form
+import play.api.libs.json.Json
+import play.api.mvc._
+
+
+object AccountController  extends Controller {
+  def index = Action {
+    Ok("Accounts COntroller")
+  }
+
+
+  def create = Action{
+    ok("Create Account")
+  }
+
+  def get = Action{
+    ok("Get Account")
+  }
+
+  def list = Action{
+    ok("List Accounts")
+  }
+
+
 
 }
