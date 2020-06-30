684710f816dac2509212e456666bf640d5356f05
Muyinda ROgers <moverr@gmail.com>
2020-06-30 20:16:50 +0300

    :Debuggging :

--- app/dto/User.scala
+++ app/dto/User.scala
@@ -1,6 +1,7 @@
 package app.dto
 
 import anorm._
+import play.db.jpa._
 
 case class User (
                   id:Long,
@@ -9,4 +10,4 @@ case class User (
                 )
 
 
-object User extends Magic[User]
+//object User extends Magic[User]
