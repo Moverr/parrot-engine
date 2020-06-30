0d863fc10099bd79090166e1faa3971bc333a348
Muyinda ROgers <moverr@gmail.com>
2020-06-30 21:36:10 +0300

    :Debuggging

--- app/dto/User.scala
+++ app/dto/User.scala
@@ -6,7 +6,11 @@ import java.util
 @Entity
 @Table(name="users")
 case class User (
+                  @Id
+                  @GeneratedValue(strategy = GenerationType.IDENTITY)
+                  @Column(name="user_id")
                   id:Long,
+                  @Column(name="email")
                   username: String,
                   password: String
                 )
