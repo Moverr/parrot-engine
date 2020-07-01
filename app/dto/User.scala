df694e4f0e7b9a5f3d9722c35da0015fe2685151
Muyinda ROgers <moverr@gmail.com>
2020-07-01 18:49:52 +0300

    Making the first JPA class  under scala

--- app/dto/User.scala
+++ app/dto/User.scala
@@ -2,18 +2,40 @@ package app.dto
 
 import javax.persistence._
 import java.util
+import com.sun.istack.internal.NotNull
 
 @Entity
-@Table(name="users")
-case class User (
-                  @Id
-                  @GeneratedValue(strategy = GenerationType.IDENTITY)
-                  @Column(name="user_id")
-                  id:Long,
-                  @Column(name="email")
-                  username: String,
-                  password: String
-                )
+@Table(name = "users")
+case class User(
+                 @Id
+                 @GeneratedValue(strategy = GenerationType.IDENTITY)
+                 @Column(name = "user_id")
+                 id: Long,
+
+
+                 @Basic(optional = false) @NotNull @Size(min = 1, max = 255)
+                 @Column(name = "username")
+                 username: String,
+                 @Basic(optional = false) @NotNull @Size(min = 1, max = 255)
+                 @Column(name = "password")
+                 password: String,
+                 @Column(name = "author_id")
+                 author_id: integer,
+
+
+                 @Basic(optional = false) @NotNull
+                 @Column(name = "created_on") @Temporal(TemporalType.TIMESTAMP)
+                 createdOn: TIMESTAMP = null,
+
+                 @Column(name = "join_date") @Temporal(TemporalType.TIMESTAMP)
+                 joinDate: TIMESTAMP = null,
+
+
+                 @JoinColumn(name = "author_id", referencedColumnName = "id") @ManyToOne(optional = true)
+                 private val author: User = null
+
+
+               )
 
 
 //object User extends Magic[User]
