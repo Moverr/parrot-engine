7f22cbb6f416ffbefa08ea53e7e403b067d874ec
Muyinda ROgers <moverr@gmail.com>
2020-07-01 23:46:45 +0300

    Debugging

--- app/dao/UserDAO.scala
+++ app/dao/UserDAO.scala
@@ -1,5 +1,12 @@
 package app.dao
 
+import app.dto.User
+
 object UserDAO {
 
+  //todo: Get Usesr by username and email
+  def getUser(username:String,password:String): List[User] ={
+
+    return null
+  }
 }
