4a650f37eb9c9ef33d703d5ce02c99f345fc9aa5
Muyinda ROgers <moverr@gmail.com>
2020-07-01 23:51:50 +0300

    :Debuggging

--- app/dao/UserDAO.scala
+++ app/dao/UserDAO.scala
@@ -1,12 +1,22 @@
 package app.dao
 
 import app.dto.User
+import play.db.jpa.JPA
+
 
 object UserDAO {
 
   //todo: Get Usesr by username and email
-  def getUser(username:String,password:String): List[User] ={
+  def getUser(username: String, password: String): List[User] = {
 
-    return null
+    return JPA.withTransaction(new play.libs.F.Function0[List[User]]() {
+      @Override
+      def apply():List[User] = {
+        return JPA.em().createQuery("from User WHERE username = :username AND password = :password").
+          setParameter("username", username).setParameter("password", password).
+          getResultList().asInstanceOf[List[User]]
+
+      }
+    })
   }
 }
