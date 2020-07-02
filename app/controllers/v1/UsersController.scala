6ddcebb1d54a8e79a279197d8e61868811a49926
Muyinda ROgers <moverr@gmail.com>
2020-07-03 01:19:31 +0300

    :Debugging

--- app/controllers/v1/UsersController.scala
+++ app/controllers/v1/UsersController.scala
@@ -1,5 +1,9 @@
 package controllers.v1
 
+import app.dao.UserDAO
+import javax.inject.Inject
+
+
 import javax.inject.Inject
 import play.api.Logger
 import play.api.data.Form
@@ -7,11 +11,9 @@ import play.api.libs.json.Json
 import play.api.mvc._
 
 
-
-
-
 object UsersController  extends Controller{
     def index = Action {
+        var userDao =   UserDAO.getUser("moeoe","mowoee");
         Ok("Users  COntroller")
     }
 
