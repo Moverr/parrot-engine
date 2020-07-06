0a64dfb81acc29fc41cb8d2127a4704ea00f0039
Muyinda ROgers <moverr@gmail.com>
2020-07-07 02:50:25 +0300

    Debugging , Authentication , Login, SOcial Auth

---
+++ app/controllers/v1/AuthController.scala
@@ -0,0 +1,22 @@
+package app.controllers.v1
+
+import app.entities.requests.{LoginRequest, SocialAuthentication}
+import javax.inject.Inject
+import play.api.Logger
+import play.api.data.Form
+import play.api.libs.json.Json
+import play.api.mvc._
+
+
+
+object AuthController extends Controller {
+
+
+    def authenticate(login:LoginRequest): Unit ={
+
+    }
+
+    def socialAuthenticate(socialauth:SocialAuthentication): Unit ={
+
+    }
+}
