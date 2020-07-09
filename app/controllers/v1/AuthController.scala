dbeef99684ee1f808f732545f0b92fb68ebec540
Muyinda ROgers <moverr@gmail.com>
2020-07-09 14:27:44 +0300

    :Debuggging

--- app/controllers/v1/AuthController.scala
+++ app/controllers/v1/AuthController.scala
@@ -3,18 +3,40 @@ package controllers.v1
 import app.entities.requests.{LoginRequest, SocialAuthentication}
 import app.entities.responses.{AuthResponse, UserResponse}
 import javax.inject.Inject
+import jdk.nashorn.internal.ir.debug.JSONWriter
 import play.api.Logger
 import play.api.data.Form
 import play.api.libs.json.Json
 import play.api.mvc._
 
 
+
 object AuthController extends Controller {
 
-    def authenticate(login: LoginRequest): AuthResponse = {
-        val userResponse = new AuthResponse("Trust in the lord", null)
-        return userResponse
+    //    def authenticate = Action{
+    //        request =>
+    //            val json = request.body.asJson.get
+    //            val userResponse = new AuthResponse("Trust in the lord", null)
+    //            ok(userResponse)
+    //    }
+    //    def authenticate(login: LoginRequest): AuthResponse = {
+    //        val userResponse = new AuthResponse("Trust in the lord", null)
+    //        return userResponse
+    //    }
+    def index = Action {
+        request =>
+            val json = request.body.asJson.get
+            val userResponse = new AuthResponse("Trust in the lord", null)
+
+           // implicit val wformats = DefaultFormats
+
+            implicit val formats =  Json.format[userResponse]
+
+            val jsonString = write(userResponse)
+
+            Ok(Json.toJson(jsonString))
     }
+
 
     def socialAuthenticate(socialauth: SocialAuthentication): Unit = {
     }
@@ -30,4 +52,6 @@ object AuthController extends Controller {
     def register(): Unit = {
 
     }
+
+
 }
