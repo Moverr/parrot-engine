965b49b93b65bfcd1bd27d1e2d553b11bc9e19e6
Muyinda ROgers <moverr@gmail.com>
2020-07-29 17:00:41 +0300

    :Debuggging

--- app/services/UsersService.scala
+++ app/services/UsersService.scala
@@ -1,6 +1,43 @@
 package app.services
 
+import java.sql.ResultSet
+
+import controllers.v1.AuthController.conn
+
+
+
+//////
+import play.api.Play.current
+import play.api.db._
+///////
+
+
+
+
+
 class UsersService extends BaseServiceTrait {
+
+  def createUser1(username:String,password:String): Boolean ={
+
+   false
+  }
+
+
+  def fetchUserByEmailAndPassword(email: String, password: String): ResultSet = {
+
+    var query = "SELECT * FROM  \"default\".users as A " +
+      "WHERE " +
+      " A.username LIKE \'" + email + "\' " +
+      "AND" +
+      " A.password LIKE \'" + password + "\' ";
+    print("STR: " + query)
+    conn = DB.getConnection()
+    val stmt = conn.createStatement
+    var resultSet = stmt.executeQuery(query)
+    resultSet
+
+  }
+
 
   override def createUser(): Unit ={
 
