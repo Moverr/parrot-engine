578f03d7286bb5ba58cb9a76d2edfaf75d4d2d70
Muyinda ROgers <moverr@gmail.com>
2020-08-02 13:59:44 +0300

    Stations Controller

--- app/controllers/v1/StationsController.scala
+++ app/controllers/v1/StationsController.scala
@@ -1,5 +1,27 @@
 package controllers.v1
 
-class StationsController {
+import play.api.mvc.{Action, Controller}
+
+//todo: create stations and move on
+class StationsController extends Controller {
+    def create = Action {
+        Ok("Interesting")
+    }
+
+    def getAll = Action {
+        Ok("List all Stations")
+    }
+
+    def get = Action {
+        Ok("Get By Id")
+    }
+
+    def archive = Action {
+        Ok("Archivee Station")
+    }
+
+    def refresh = Action {
+        Ok("Refresh Station to Active")
+    }
 
 }
