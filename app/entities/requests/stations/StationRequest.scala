cdbd4383854b7c438be97ca6114096cc0d108ff1
Muyinda ROgers <moverr@gmail.com>
2020-08-03 15:26:04 +0300

    StationRequest

--- app/entities/requests/stations/StationRequest.scala
+++ app/entities/requests/stations/StationRequest.scala
@@ -1,3 +1,18 @@
 package entities.requests.stations
 
-case class StationRequest()
+import play.api.data.Form
+import play.api.data.Forms._
+
+
+case class StationRequest(name: String, code: String)
+
+object StationRequest {
+  val form: Form[StationRequest] = Form(
+    mapping(
+      "name" -> nonEmptyText
+      , "code" -> nonEmptyText
+    )(StationRequest.apply)(StationRequest.unapply)
+  )
+}
+
+
