35506e1f72dc852d808d9384c73335cd4063d95f
Muyinda ROgers <moverr@gmail.com>
2020-08-03 22:12:59 +0300

    :Debuggging

--- app/services/AccountService.scala
+++ app/services/AccountService.scala
@@ -32,81 +32,6 @@ class AccountService {
 
   }
 
-
-  def getAll(owner: Integer, limit: Integer = 0, offset: Integer = 10): Unit = {
-    //todo: verify that owner is not null
-    if (owner == null) BadRequest(Json.obj("status" -> "Error", "message" -> "Invalid Authentication"))
-
-    val external_id = HelperUtilities.randomStringGenerator(20)
-    if (checkIfAccountExists(owner) == true) {
-      BadRequest(Json.obj("status" -> "Error", "message" -> "Account already created for user"))
-    } else {
-      var query = "INSERT INTO  \"default\".account (owner,name,external_id,author_id)  values ('" + owner + "','" + account.name + "','" + external_id + "','" + owner + "') ";
-      conn = DB getConnection()
-      val stmt = conn createStatement
-      var result = stmt executeUpdate (query)
-    }
-
-  }
-
-
-
-  def getById(owner: Integer, stationId: Integer ): Unit = {
-    //todo: verify that owner is not null
-    if (owner == null) BadRequest(Json.obj("status" -> "Error", "message" -> "Invalid Authentication"))
-
-    val external_id = HelperUtilities.randomStringGenerator(20)
-    if (checkIfAccountExists(owner) == true) {
-      BadRequest(Json.obj("status" -> "Error", "message" -> "Account already created for user"))
-    } else {
-      var query = "INSERT INTO  \"default\".account (owner,name,external_id,author_id)  values ('" + owner + "','" + account.name + "','" + external_id + "','" + owner + "') ";
-      conn = DB getConnection()
-      val stmt = conn createStatement
-      var result = stmt executeUpdate (query)
-    }
-
-  }
-
-
-
-  def Archive(owner: Integer, stationId: Integer ): Unit = {
-    //todo: verify that owner is not null
-    if (owner == null) BadRequest(Json.obj("status" -> "Error", "message" -> "Invalid Authentication"))
-
-    val external_id = HelperUtilities.randomStringGenerator(20)
-    if (checkIfAccountExists(owner) == true) {
-      BadRequest(Json.obj("status" -> "Error", "message" -> "Account already created for user"))
-    } else {
-      var query = "INSERT INTO  \"default\".account (owner,name,external_id,author_id)  values ('" + owner + "','" + account.name + "','" + external_id + "','" + owner + "') ";
-      conn = DB getConnection()
-      val stmt = conn createStatement
-      var result = stmt executeUpdate (query)
-    }
-
-  }
-
-
-
-
-  def Activate(owner: Integer, stationId: Integer ): Unit = {
-    //todo: verify that owner is not null
-    if (owner == null) BadRequest(Json.obj("status" -> "Error", "message" -> "Invalid Authentication"))
-
-    val external_id = HelperUtilities.randomStringGenerator(20)
-    if (checkIfAccountExists(owner) == true) {
-      BadRequest(Json.obj("status" -> "Error", "message" -> "Account already created for user"))
-    } else {
-      var query = "INSERT INTO  \"default\".account (owner,name,external_id,author_id)  values ('" + owner + "','" + account.name + "','" + external_id + "','" + owner + "') ";
-      conn = DB getConnection()
-      val stmt = conn createStatement
-      var result = stmt executeUpdate (query)
-    }
-
-  }
-
-
-
-
   def checkIfAccountExists(owner: Integer): Boolean = {
     var query = "SELECT * FROM   \"default\".account WHERE owner = " + owner + "    ) ";
     conn = DB getConnection()
@@ -120,6 +45,7 @@ class AccountService {
     conn = DB getConnection()
     val stmt = conn createStatement
     var result = stmt.executeQuery(query)
+
 
 
     var account = new AccountResponse("");
--- app/services/StationsService.scala
+++ app/services/StationsService.scala
@@ -2,6 +2,7 @@ package services
 
 import controllers.v1.AuthController.{BadRequest, conn}
 import entities.requests.accounts.AccountReqquest
+import entities.responses.accounts.AccountResponse
 import play.api.db.DB
 import play.api.libs.json.Json
 import utils.HelperUtilities
@@ -13,6 +14,7 @@ import play.api.Play.current
 
 
 class StationsService {
+  //todo: create
   def create(owner: Integer, account: AccountReqquest): Unit = {
     //todo: verify that owner is not null
     if (owner == null) BadRequest(Json.obj("status" -> "Error", "message" -> "Invalid Authentication"))
@@ -30,12 +32,103 @@ class StationsService {
 
   }
 
+
+  def getAll(owner: Integer, limit: Integer = 0, offset: Integer = 10): Unit = {
+    //todo: verify that owner is not null
+    if (owner == null) BadRequest(Json.obj("status" -> "Error", "message" -> "Invalid Authentication"))
+
+    val external_id = HelperUtilities.randomStringGenerator(20)
+    if (checkIfAccountExists(owner) == true) {
+      BadRequest(Json.obj("status" -> "Error", "message" -> "Account already created for user"))
+    } else {
+      var query = "INSERT INTO  \"default\".account (owner,name,external_id,author_id)  values ('" + owner + "','" + account.name + "','" + external_id + "','" + owner + "') ";
+      conn = DB getConnection()
+      val stmt = conn createStatement
+      var result = stmt executeUpdate (query)
+    }
+
+  }
+
+
+
+  def getById(owner: Integer, stationId: Integer ): Unit = {
+    //todo: verify that owner is not null
+    if (owner == null) BadRequest(Json.obj("status" -> "Error", "message" -> "Invalid Authentication"))
+
+    val external_id = HelperUtilities.randomStringGenerator(20)
+    if (checkIfAccountExists(owner) == true) {
+      BadRequest(Json.obj("status" -> "Error", "message" -> "Account already created for user"))
+    } else {
+      var query = "INSERT INTO  \"default\".account (owner,name,external_id,author_id)  values ('" + owner + "','" + account.name + "','" + external_id + "','" + owner + "') ";
+      conn = DB getConnection()
+      val stmt = conn createStatement
+      var result = stmt executeUpdate (query)
+    }
+
+  }
+
+
+
+  def Archive(owner: Integer, stationId: Integer ): Unit = {
+    //todo: verify that owner is not null
+    if (owner == null) BadRequest(Json.obj("status" -> "Error", "message" -> "Invalid Authentication"))
+
+    val external_id = HelperUtilities.randomStringGenerator(20)
+    if (checkIfAccountExists(owner) == true) {
+      BadRequest(Json.obj("status" -> "Error", "message" -> "Account already created for user"))
+    } else {
+      var query = "INSERT INTO  \"default\".account (owner,name,external_id,author_id)  values ('" + owner + "','" + account.name + "','" + external_id + "','" + owner + "') ";
+      conn = DB getConnection()
+      val stmt = conn createStatement
+      var result = stmt executeUpdate (query)
+    }
+
+  }
+
+
+
+
+  def Activate(owner: Integer, stationId: Integer ): Unit = {
+    //todo: verify that owner is not null
+    if (owner == null) BadRequest(Json.obj("status" -> "Error", "message" -> "Invalid Authentication"))
+
+    val external_id = HelperUtilities.randomStringGenerator(20)
+    if (checkIfAccountExists(owner) == true) {
+      BadRequest(Json.obj("status" -> "Error", "message" -> "Account already created for user"))
+    } else {
+      var query = "INSERT INTO  \"default\".account (owner,name,external_id,author_id)  values ('" + owner + "','" + account.name + "','" + external_id + "','" + owner + "') ";
+      conn = DB getConnection()
+      val stmt = conn createStatement
+      var result = stmt executeUpdate (query)
+    }
+
+  }
+
+
+
+
   def checkIfAccountExists(owner: Integer): Boolean = {
     var query = "SELECT * FROM   \"default\".account WHERE owner = " + owner + "    ) ";
     conn = DB getConnection()
     val stmt = conn createStatement
     var result = stmt.executeQuery(query)
     if (result.next()) true else false
+  }
+
+  def get(owner: Integer): AccountResponse = {
+    var query = "SELECT * FROM   \"default\".account WHERE owner = " + owner + " ";
+    conn = DB getConnection()
+    val stmt = conn createStatement
+    var result = stmt.executeQuery(query)
+
+
+    var account = new AccountResponse("");
+    if (result.next()) {
+
+      val accountName = result.getString("name");
+      account = new AccountResponse(accountName);
+    }
+    account
   }
 
 }
