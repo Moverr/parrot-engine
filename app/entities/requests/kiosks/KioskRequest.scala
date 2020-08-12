bbcfbdb99a0b7aaac04de8eb9a45600c8f7ebcce
Muyinda ROgers <moverr@gmail.com>
2020-08-12 14:14:26 +0300

    :Debuggging

--- app/entities/requests/kiosks/KioskRequest.scala
+++ app/entities/requests/kiosks/KioskRequest.scala
@@ -1,3 +1,22 @@
 package entities.requests.kiosks
 
-case class KioskRequest()
+import play.api.data.Form
+import play.api.data.Forms._
+import play.api.data.format.Formats._
+
+case class KioskRequest(station_id:Long,reference_id:String,device_token:String,details:String)
+
+
+
+object KioskRequest {
+  val form: Form[KioskRequest] = Form(
+    mapping(
+      "station_id" -> of[Long]
+      , "reference_id" -> of[String]
+      , "device_token" -> of[String]
+      , "details" -> of[String]
+    )(KioskRequest.apply)(KioskRequest.unapply)
+  )
+}
+
+
