0e3c196183b68c030fe75372530a148dedba7480
Muyinda ROgers <moverr@gmail.com>
2020-11-15 02:04:55 +0300

    :Debuggging

--- app/tables/Main.scala
+++ app/tables/Main.scala
@@ -5,23 +5,35 @@ package tables
 //import scala.concurrent.ExecutionContext.Implicits.global
 //import slick.driver.PostgresDriver.api._
 
+import play.data.format.Formats.DateTime
 import slick.driver.PostgresDriver.api._
+
 //import slick.driver.pos.
 object Main {
 
-  case class  Account(
-                       owner:Long,
-                       name:String,
-                       external_id:String,
-                       author_id:Long=0L
-                     )
-  class  AccountTable(tag: Tag) extends Table[Account](tag,"accounts"){
+  case class Account(
+                      id: Long = 0L,
+                      owner: Long,
+                      name: String,
+                      created_on: DateTime,
+                      updated_on: DateTime,
+                      author_id: Long,
+                      updated_by: Long,
+                      external_id: String
+
+                    )
+
+  class AccountTable(tag: Tag) extends Table[Account](tag, "account") {
     def owner = column[Long]("owner")
-    def name  = column[String]("name")
-    def external_id  = column[String]("external_id")
-    def author_id     = column[Long]("author_id")
+
+    def name = column[String]("name")
+
+    def external_id = column[String]("external_id")
+
+    def author_id = column[Long]("author_id")
 
 
-    def * = (owner, name, external_id,author_id) <> (Account.tupled, Account.unapply)
+    def * = (owner, name, external_id, author_id) <> (Account.tupled, Account.unapply)
   }
+
 }
