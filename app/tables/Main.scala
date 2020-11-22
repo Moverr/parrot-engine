5ce90ba6d2fd2f33c06c43db83fa68e6a8bda6e6
Muyinda ROgers <moverr@gmail.com>
2020-11-22 03:53:45 +0300

    :Debuggging

--- app/tables/Main.scala
+++ app/tables/Main.scala
@@ -64,4 +64,34 @@ object Main {
 
   lazy  val DepartmentTable = TableQuery[DepartmentTable]
 
+
+
+
+  //todo: Employee  table
+  case class Employee(
+                         id: Long = 0L,
+                         name: String,
+                         gender: String,
+                         created_on: DateTime,
+                         updated_on:Option[DateTime],
+                         author_id: Long,
+                         updated_by: Long
+                       )
+
+  class EmployeeEmployee(tag:Tag) extends Table[Employee](tag,"employees"){
+    def id = column[Long]("id",O.PrimaryKey,O.AutoInc)
+    def name = column[String]("names")
+    def gender  = column[String]("gender")
+
+    def created_on = column[DateTime]("created_on")
+    def updated_on = column[DateTime]("updated_on")
+    def author_id = column[Long]("author_id")
+    def updated_by = column[Long]("updated_by")
+
+    override def * = (id,name,gender,created_on,updated_on.?,author_id,updated_by) <> (Employee.tupled,Employee.apply())
+  }
+
+  lazy  val EmployeeEmployee = TableQuery[EmployeeEmployee]
+
+
 }
