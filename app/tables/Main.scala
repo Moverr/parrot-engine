11b89ea31298a1b25a046d2629254230d63e762c
Muyinda ROgers <moverr@gmail.com>
2020-11-24 01:23:07 +0300

    :Debugging

--- app/tables/Main.scala
+++ app/tables/Main.scala
@@ -358,4 +358,28 @@ object Main {
 
 
 
+
+
+
+  //todo: Stations    table
+  case class Station(
+                   id: Long = 0L,
+                   account_id:Long,
+                   name: String,
+                   code:String
+                 )
+
+  class StationTable(tag:Tag) extends Table[Station](tag,"stations"){
+    override def * = (id,account_id,name,code) <> (Station.tupled,Station.unapply)
+
+    def id = column[Long]("id",O.PrimaryKey,O.AutoInc)
+    def account_id = column[Long]("account_id")
+
+    def name = column[String]("name")
+    def code = column[String]("code")
+
+  }
+
+
+
 }
