7ff5c61d2a598f66aa5e4b09fd75e77ceb488e3e
Muyinda ROgers <moverr@gmail.com>
2020-11-23 20:48:44 +0300

    :Debuggging

--- app/tables/Main.scala
+++ app/tables/Main.scala
@@ -197,6 +197,25 @@ object Main {
 
 
 
+  //todo: Office Station   table
+  case class OfficeStation(
+                              id: Long = 0L,
+                              office_id: Long,
+                              station_id: Long
+                            )
+
+  class OfficeStationTable(tag:Tag) extends Table[OfficeStation](tag,"office_station"){
+    def id = column[Long]("id",O.PrimaryKey,O.AutoInc)
+    def office_id = column[Long]("office_id")
+    def station_id = column[Long]("station_id")
+
+    override def * = (id,office_id,station_id) <> (OfficeStation.tupled,OfficeStation.unapply)
+  }
+
+  lazy  val OfficeStationTable = TableQuery[OfficeStationTable]
+
+
+
 
 
 }
