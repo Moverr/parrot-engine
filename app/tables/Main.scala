bb6b951ad16e9e684a888493baeb56ea4f45c5ca
Muyinda ROgers <moverr@gmail.com>
2020-11-24 00:57:50 +0300

    :Debuggging

--- app/tables/Main.scala
+++ app/tables/Main.scala
@@ -300,6 +300,7 @@ object Main {
                       code:String,
                       name: String,
                       is_system: Boolean,
+
                       author_id:Long,
                       created_on: DateTime,
                       updated_by: Long,
@@ -308,11 +309,9 @@ object Main {
 
   class RoleTable(tag:Tag) extends Table[Role](tag,"role"){
     def id = column[Long]("id",O.PrimaryKey,O.AutoInc)
-    def user_id = column[Long]("user_id")
     def code = column[String]("code")
     def name = column[String]("name")
     def is_system = column[Boolean]("is_system")
-
 
 
 
@@ -326,10 +325,10 @@ object Main {
 
 
 
-    override def * = (id,user_id,code,name,author_id,created_on,updated_by,updated_on.?) <> (Profile.tupled,Profile.unapply)
+    override def * = (id,code,name,is_system,author_id,created_on,updated_by,updated_on.?) <> (Role.tupled,Role.unapply)
   }
 
-  lazy  val ProfileTable = TableQuery[ProfileTable]
+  lazy  val RoleTable = TableQuery[RoleTable]
 
 
 
