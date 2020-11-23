3d689358fb7a0824d769b2fb8823af29a5c6c1e1
Muyinda ROgers <moverr@gmail.com>
2020-11-24 01:40:25 +0300

    :Debuggging

--- app/tables/Main.scala
+++ app/tables/Main.scala
@@ -386,8 +386,9 @@ object Main {
   //todo:   user   Roles  table
   case class UserRole(
                              id: Long = 0L,
+                             user_id: Long,
                              role_id: Long,
-                             permission_id: Long,
+                             status: String,
 
                              author_id:Long,
                              created_on: DateTime,
@@ -396,13 +397,14 @@ object Main {
                            )
 
   class UserRoleTable(tag:Tag) extends Table[UserRole](tag,"user_role"){
-    override def * = (id,role_id,permission_id,author_id,created_on,updated_by,updated_on.?) <> (UserRole.tupled,UserRole.unapply)
+    override def * = (id,user_id,role_id,status,author_id,created_on,updated_by,updated_on.?) <> (UserRole.tupled,UserRole.unapply)
 
     def id = column[Long]("id",O.PrimaryKey,O.AutoInc)
 
-    def role_id = column[Long]("role_id")
+    def user_id = column[Long]("user_id")
 
-    def permission_id = column[Long]("station_id")
+    def role_id = column[Long]("role_id")
+    def status = column[String]("status")
 
     def created_on = column[DateTime]("created_on")
 
