88cab77167085d2289db492933e678e472b7b5ab
Muyinda ROgers <moverr@gmail.com>
2020-11-23 18:22:17 +0300

    :Debugging

--- app/tables/Main.scala
+++ app/tables/Main.scala
@@ -113,21 +113,46 @@ object Main {
 
   lazy  val EmployeeStationTable = TableQuery[EmployeeStationTable]
 
-  /*
   //todo: Kiosk  table
   case class Kiosk(
-                       id: Long = 0L,
-                       reference: String,
-                       details: String,
-                       token: String,
-                       station: Long,
-                       created_on: DateTime,
-                       updated_on:DateTime,
-                       author_id: Long,
-                       updated_by:Long ,
-                       station_id:Long
+                    id: Long = 0L,
+                    reference: String,
+                    details: String,
+                    token: String,
+                    station: Long,
+                    created_on: DateTime,
+                    updated_on:DateTime,
+                    author_id: Long,
+                    updated_by:Long ,
+                    station_id:Long
 
-                     )
+                  )
+
+
+  class KioskTable(tag:Tag) extends Table[Kiosk](tag,"kiosks") {
+    def id = column[Long]("id", O.PrimaryKey, O.AutoInc)
+
+    def reference = column[String]("reference")
+
+    def details = column[String]("details")
+
+    def device_token = column[String]("device_token")
+
+    def created_on = column[DateTime]("created_on")
+
+    def updated_on = column[DateTime]("updated_on")
+
+    def author_id = column[Long]("author_id")
+
+    def updated_by = column[Long]("updated_by")
+
+    def station_id = column[Long]("station_id")
+
+    override def * = (id,reference,details,device_token,created_on,updated_on,author_id,updated_by,station_id) <> (Kiosk.tupled,Kiosk.unapply)
+
+  }
+
+  /*
 
   class KioskTable(tag:Tag) extends Table[Kiosk](tag,"kiosks"){
     def id = column[Long]("id",O.PrimaryKey,O.AutoInc)
