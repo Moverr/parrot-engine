b74c7ca11d7e0ed135846f47ea194dd6a38101e2
Muyinda ROgers <moverr@gmail.com>
2020-12-14 14:15:31 +0300

    :Debuggging :Station

--- app/tables/Main.scala
+++ app/tables/Main.scala
@@ -37,32 +37,6 @@ object Main {
 
 
 
-
-
-
-
-  //todo: Stations    table
-  case class Station(
-                      id: Long = 0L,
-                      account_id: Long,
-                      name: String,
-                      code: String
-                    )
-
-  class StationTable(tag: Tag) extends Table[Station](tag, "stations") {
-    override def * = (id, account_id, name, code) <> (Station.tupled, Station.unapply)
-
-    def id = column[Long]("id", O.PrimaryKey, O.AutoInc)
-
-    def account_id = column[Long]("account_id")
-
-    def name = column[String]("name")
-
-    def code = column[String]("code")
-
-  }
-
-
   //todo:   user   Roles  table
   case class UserRole(
                        id: Long = 0L,
--- app/tables/RolePermission.scala
+++ app/tables/RolePermission.scala
@@ -1,7 +1,6 @@
 package tables
 
 import org.joda.time.DateTime
-import org.joda.time.DateTime
 import slick.jdbc.PostgresProfile.api._
 //import defaults ::
 import implicits.CustomColumnTypes._
