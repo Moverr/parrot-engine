b7bb3d44f930da03afe7d43766eec608679c0749
Muyinda ROgers <moverr@gmail.com>
2020-12-14 14:13:35 +0300

    :Debuggging : Roles

--- app/tables/Main.scala
+++ app/tables/Main.scala
@@ -50,37 +50,7 @@ object Main {
 
 
 
-  //todo: Roles    table
-  case class Role(
-                   id: Long = 0L,
-                   code: String,
-                   name: String,
-                   is_system: Boolean,
-                   author_id: Long,
-                   created_on: DateTime,
-                   updated_by: Long,
-                   updated_on: Option[DateTime]
-                 )
 
-  class RoleTable(tag: Tag) extends Table[Role](tag, "role") {
-    override def * = (id, code, name, is_system, author_id, created_on, updated_by, updated_on.?) <> (Role.tupled, Role.unapply)
-
-    def id = column[Long]("id", O.PrimaryKey, O.AutoInc)
-
-    def code = column[String]("code")
-
-    def name = column[String]("name")
-
-    def is_system = column[Boolean]("is_system")
-
-    def created_on = column[DateTime]("created_on")
-
-    def updated_on = column[DateTime]("dateupdated")
-
-    def author_id = column[Long]("author_id")
-
-    def updated_by = column[Long]("updated_by")
-  }
 
   //todo: Roles  Permissions    table
   case class RolePermission(
