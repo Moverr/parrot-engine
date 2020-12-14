5ef0a4286e2b189bc246a2adc98122a7151e196a
Muyinda ROgers <moverr@gmail.com>
2020-12-14 14:03:47 +0300

    Debuggigng

--- app/tables/Main.scala
+++ app/tables/Main.scala
@@ -41,42 +41,6 @@ object Main {
 
 
 
-  //todo: Kiosk  table
-  case class Kiosk(
-                    id: Long = 0L,
-                    reference: String,
-                    details: String,
-                    token: String,
-                    station: Long,
-                    created_on: DateTime,
-                    updated_on: Option[DateTime],
-                    author_id: Long,
-                    updated_by: Long
-
-                  )
-
-  class KioskTable(tag: Tag) extends Table[Kiosk](tag, "kiosks") {
-    override def * = (id, reference, details, token, station, created_on, updated_on.?, author_id, updated_by) <> (Kiosk.tupled, Kiosk.unapply)
-
-    def id = column[Long]("id", O.PrimaryKey, O.AutoInc)
-
-    def reference = column[String]("reference")
-
-    def details = column[String]("details")
-
-    def token = column[String]("device_token")
-
-    def station = column[Long]("station_id")
-
-    def created_on = column[DateTime]("created_on")
-
-    def updated_on = column[DateTime]("updated_on")
-
-    def author_id = column[Long]("author_id")
-
-    def updated_by = column[Long]("updated_by")
-
-  }
 
   //todo: Offices
   case class Office(
