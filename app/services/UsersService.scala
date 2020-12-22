fcf24c7cc25b51131a774942d7b4bbb94074ea73
Muyinda ROgers <moverr@gmail.com>
2020-12-22 21:03:59 +0300

    :Debugggng

--- app/services/UsersService.scala
+++ app/services/UsersService.scala
@@ -6,13 +6,12 @@ import app.entities.responses.AuthResponse
 import controllers.v1.AuthController.BadRequest
 import entities.responses.RegistrationResponse
 import javax.inject.{Inject, Singleton}
-import org.joda.time.DateTime
 import play.api.db.slick.DatabaseConfigProvider
 import play.api.libs.json.Json
 import services.traits.TUserService
 import slick.jdbc.JdbcProfile
 import slick.lifted
-import tables.User
+import tables.{User, UserTable}
 import utils.{HelperUtilities, PasswordHashing}
 
 import scala.concurrent.Future
@@ -20,8 +19,6 @@ import scala.concurrent.Future
 import scala.util.Success
 //////
 //import defaults ::
-import implicits.CustomColumnTypes._
-
 import slick.jdbc.PostgresProfile.api._
 ///////
 import scala.concurrent.ExecutionContext.Implicits.global
@@ -144,24 +141,6 @@ class UsersService @Inject()(
   }
 
 
-
-  private class UserTable(tag: Tag) extends Table[User](tag, "user") {
-    override def * = (id, username, password, author_id, created_on, updated_by, updated_on.?) <> (User.tupled, User.unapply)
-
-    def id = column[Long]("id", O.PrimaryKey, O.AutoInc)
-
-    def username = column[String]("username")
-
-    def password = column[String]("password")
-
-    def created_on = column[DateTime]("created_on")
-
-    def updated_on = column[DateTime]("dateupdated")
-
-    def author_id = column[Long]("author_id")
-
-    def updated_by = column[Long]("updated_by")
-  }
 
 }
 
