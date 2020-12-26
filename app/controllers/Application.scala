9e4407e48afb88fd0b120663942fac148ff00f18
Muyinda ROgers <moverr@gmail.com>
2020-12-26 04:49:01 +0300

    :Debuggging

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,13 +1,13 @@
 package controllers
 
-import javax.inject.{Inject, Singleton}
 import play.api.mvc._
 
 
-@Singleton
- class Application  @Inject()(cc: ControllerComponents)  extends AbstractController(cc) {
+ object Application  extends Controller {
   def index = Action {
    val message:String = "PARROT VERSION 1.0.0"
+
+
     Ok(views.html.index(message))
   }
 }
